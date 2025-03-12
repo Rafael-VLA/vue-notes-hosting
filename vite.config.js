5f2fcdba3887bf884d8145a3c4763c9bae9a1513
Rafael-VLA <Rafaelsequeira@vlacademy.com>
2025-03-11 21:37:31 -0600

    Added config

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,19 @@ import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+
+
+  return {
+    base: mode === 'development' ? "/" : "/vue-notes-hosting",
+    plugins: [
+      vue(),
+      // vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
